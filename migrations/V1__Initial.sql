d62fc9ca616442a208c20ecc1b448a2941c479d5
Max N <max.naluparayil@cgi.com>
2022-02-22 14:29:53 -0500

    asdf

--- migrations/V1__Initial.sql
+++ migrations/V1__Initial.sql
@@ -1,5 +1,5 @@
 CREATE TABLE HelloWorld (
-    HelloWorldId INT IDENTITY ,
+    HelloWorldId INT IDENTITY,
     Declaration nvarchar(1000) NULL,
     CONSTRAINT PK_HelloWorld PRIMARY KEY CLUSTERED (HelloWorldId)
 );
